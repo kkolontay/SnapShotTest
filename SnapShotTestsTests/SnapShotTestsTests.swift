064cae456c9f1a48c9d1a2e0639183ffdbdaaf1c
Kostiantyn Kolontai <kkolontay@gmail.com>
2025-11-25 22:31:10 -0500

    help

--- SnapShotTestsTests/SnapShotTestsTests.swift
+++ SnapShotTestsTests/SnapShotTestsTests.swift
@@ -13,12 +13,6 @@ import SwiftUI
 // MARK: - Snapshot Tests
 class SnapShotTestsTests: XCTestCase {
 
-  override func invokeTest() {
-    withSnapshotTesting(record: .failed) {
-      super.invokeTest()
-    }
-  }
-
   @MainActor
   func testSnapshot_mainContent_iPhone13() {
     let vm = ViewStore()
@@ -66,24 +60,6 @@ class SnapShotTestsTests: XCTestCase {
   }
 
   @MainActor
-  func testSnapshot_sheetContent_iPhone13() {
-    let vm = ViewStore()
-    let sheetView = SheetContentView(viewModel: vm)
-    let view = UINavigationController(rootViewController: UIHostingController(rootView: sheetView))
-
-    assertSnapshot(of: view, as: .wait(for: 0.3, on: .image(on: .iPhone13)))
-  }
-
-  @MainActor
-  func testSnapshot_mainContent_iPhoneSE() {
-    let vm = ViewStore()
-    let viewS = ContentView(viewModel: vm)
-    let view = UINavigationController(rootViewController: UIHostingController(rootView:viewS))
-
-    assertSnapshot(of: view, as: .wait(for: 0.3, on: .image(on: .iPhoneSe)))
-  }
-
-  @MainActor
   func testSnapshot_withSheet_iPhoneSE() {
     let vm = ViewStore()
     vm.show = true
@@ -113,24 +89,6 @@ class SnapShotTestsTests: XCTestCase {
     assertSnapshot(of: navController, as: .image(on: .iPhoneSe, traits: .init(userInterfaceStyle: .light)))
 
     window.isHidden = true
-  }
-
-  @MainActor
-  func testSnapshot_sheetContent_iPhoneSE() {
-    let vm = ViewStore()
-    let sheetView = SheetContentView(viewModel: vm)
-    let view = UINavigationController(rootViewController: UIHostingController(rootView: sheetView))
-
-    assertSnapshot(of: view, as: .wait(for: 0.3, on: .image(on: .iPhoneSe)))
-  }
-
-  @MainActor
-  func testSnapshot_mainContent_iPadMini() {
-    let vm = ViewStore()
-    let viewS = ContentView(viewModel: vm)
-    let view = UINavigationController(rootViewController: UIHostingController(rootView:viewS))
-
-    assertSnapshot(of: view, as: .wait(for: 0.3, on: .image(on: .iPadMini(.portrait))))
   }
 
   @MainActor
